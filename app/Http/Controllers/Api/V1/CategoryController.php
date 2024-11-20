917357e14a44051aab53e6be3945b672717d98cc
Anatoly Batov <play.hoffa@gmail.com>
2024-11-20 09:21:34 +0400

    first commit

---
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        return Category::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+
+        // Обновление полей категории без валидации  
+        $category->title = $request->input('title');
+        // Добавьте здесь обновление других полей, если необходимо  
+
+        // Сохраняем изменения в базе данных  
+        $category->save();
+
+        // Возвращаем ответ (например, JSON ответ)  
+        return response()->json(['message' => 'Категория успешно обновлена.', 'category' => $category], 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        // Удаляем категорию из базы данных  
+        $category->delete();
+
+        // Возвращаем ответ (например, редирект или JSON ответ)  
+        return response()->json(['message' => 'Категория успешно удалена.'], 200);
+    }
+}
