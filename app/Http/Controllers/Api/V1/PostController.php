a5bb9f05a600bb163b9cf7f6aad3573e89361dc3
Anatoly Batov <play.hoffa@gmail.com>
2024-11-20 13:14:48 +0400

    api

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        return Post::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        print_r($post->id); die;
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        
+        // Обновление полей категории без валидации  
+        $post->update($request->all());
+
+        // Возвращаем ответ (например, JSON ответ)  
+        return response()->json(['message' => 'Категория успешно обновлена.', 'category' => $post], 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+         // Удаляем категорию из базы данных  
+         $post->delete();
+
+         // Возвращаем ответ (например, редирект или JSON ответ)  
+         return response()->json(['message' => 'Категория успешно удалена.'], 200);
+    
+    }
+}
